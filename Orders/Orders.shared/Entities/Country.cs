4a8489e0c4eac17b4addb94922f34347bcfad689
guisaodani <guisaodani@gmail.com>
2025-08-26 23:47:52 -0500

    se crea la base de datos con la tabla contries

---
+++ Orders/Orders.shared/Entities/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Orders.shared.Entities;
+
+public class Country
+{
+    public int Id { get; set; }
+
+    [Display(Name = "Pais")]
+    [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres.")]
+    [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    public string Name { get; set; } = null!;
+}
