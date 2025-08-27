12882223f97b31794b16bd3a21ee276aa1772512
guisaodani <guisaodani@gmail.com>
2025-08-27 00:06:40 -0500

    se crea controlador contries

---
+++ Orders/Orders.Backend/Controllers/CountriesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Orders.Backend.Data;
+using Orders.shared.Entities;
+
+namespace Orders.Backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CountriesController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public CountriesController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync(Country country)
+    {
+        _context.Add(country);
+        await _context.SaveChangesAsync();
+        return Ok(country);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAsync()
+    {
+        return Ok(await _context.Countries.ToListAsync());
+    }
+}
